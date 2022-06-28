ca97b60fbac67b686ced6d546951ceb99c6971a6
RomanRybnikov <rama67rus@gmail.com>
2022-06-28 17:13:36 +0300

    fix code

--- src/test_items.py
+++ src/test_items.py
@@ -6,4 +6,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_button(browser):
     browser.get(link)
     time.sleep(30)
-    assert browser.find_element(By.XPATH, "//button[@type]"), "Button not found"
+    assert browser.find_element(By.CSS_SELECTOR, ".product_page #add_to_basket_form .btn-add-to-basket"), "Button not found"
